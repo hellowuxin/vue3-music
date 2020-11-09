a07a107ecc119bf2952d2d155bd7673555b0e254
hellowuxin <hello_wuxin@qq.com>
2020-11-09 16:58:36 +0800

    初始

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <global-header></global-header>
+  <carousel></carousel>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import GlobalHeader from './components/GlobalHeader.vue'
+// import HomeView from './views/HomeView.vue'
+import Carousel from './components/Carousel.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    GlobalHeader, Carousel
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav>
+    <div class="container">
+      <ul class="nav-ul">
+        <li class="nav-li"><a class="active" href="#">个性推荐</a></li>
+        <li class="nav-li"><a href="#">歌单</a></li>
+        <li class="nav-li"><a href="#">主播电台</a></li>
+        <li class="nav-li"><a href="#">排行榜</a></li>
+        <li class="nav-li"><a href="#">歌手</a></li>
+        <li class="nav-li"><a href="#">最新音乐</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'GlobalHeader'
+})
+</script>
+
+<style lang="scss">
+$black: #000;
+
+nav {
+  background-color: #F9F9F9;
+}
+
+.container {
+  display: flex;
+  padding: 0 calc(1.5rem / 2);
+}
+
+.nav-ul {
+  flex: auto;
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+  padding: 0;
+}
+
+.nav-li {
+  a {
+    color: rgba($black, .55);
+    text-decoration: none;
+
+    &.active {
+      color: $black;
+    }
+
+    &:hover {
+      color: $black;
+    }
+  }
+}
+</style>
