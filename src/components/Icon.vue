2bd7a5fc7f27092c3cfe4461ff00e58f3476ad80
hellowuxin <hello_wuxin@qq.com>
2020-11-11 21:37:44 +0800

    icon

---
+++ src/components/Icon.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg class="icon" aria-hidden="true">
+    <use :xlink:href="`#${iconId}`"></use>
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Icon',
+  props: {
+    iconId: String
+  }
+})
+</script>
