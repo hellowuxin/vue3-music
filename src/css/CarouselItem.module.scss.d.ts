b9e078523be6878aa08feb5fe86d681d4384e678
hellowuxin <hello_wuxin@qq.com>
2020-11-19 13:57:58 +0800

    tsm

---
+++ src/css/CarouselItem.module.scss.d.ts
@@ -0,0 +1,6 @@
+export const active: string
+export const activeToDeactive: string
+export const container: string
+export const deactive: string
+export const next: string
+export const prev: string
