378036cad256915562b555ee51716c1b29afc4dd
hellowuxin <hello_wuxin@qq.com>
2020-11-22 21:36:46 +0800

    tab-item

---
+++ src/components/TabItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, useCssModule } from 'vue'
+
+export default defineComponent({
+  name: 'TabsItems',
+  setup () {
+    const style = useCssModule()
+
+    return {
+      style
+    }
+  }
+})
+</script>
+
+<style lang="scss" module>
+
+</style>
---
+++ src/components/TabsItems.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="style['container']">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, useCssModule } from 'vue'
+
+export default defineComponent({
+  name: 'TabsItems',
+  setup () {
+    const style = useCssModule()
+
+    return {
+      style
+    }
+  }
+})
+</script>
+
+<style lang="scss" module>
+.container {
+  display: flex;
+}
+</style>
