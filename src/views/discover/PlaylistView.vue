19100af459cf4dc63b13d266d0161831eca55403
hellowuxin <hello_wuxin@qq.com>
2021-03-06 21:28:35 +0800

    发现歌单页面

--- src/views/discover/PlaylistView.vue
+++ src/views/discover/PlaylistView.vue
@@ -1,23 +1,55 @@
 <template>
   <div :class="style['container']">
-    DiscoverPlaylistView
+    <div v-if="playlistArr" :class="style['content']">
+      <card
+        play
+        v-for="playlist in playlistArr"
+        :key="playlist.id"
+        :title="playlist.name"
+        :playCount="playlist.playCount"
+        :imgSrc="playlist.coverImgUrl"
+      ></card>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, useCssModule } from 'vue'
+import { defineComponent, Ref, ref, useCssModule } from 'vue'
+import axios from 'axios'
+import Card from '@/components/Card.vue'
+import { Playlist } from '@/interface'
 
 export default defineComponent({
   name: 'DiscoverPlaylistView',
+  components: {
+    Card
+  },
   setup () {
     const style = useCssModule()
+    const playlistArr: Ref<Playlist[] | undefined> = ref()
+
+    axios.get('/playlist/catlist').then(({ data }) => {
+      console.log(data)
+    })
+    axios.get('/playlist/hot').then(({ data }) => {
+      console.log(data)
+    })
+    axios.get('/top/playlist').then(({ data }) => {
+      playlistArr.value = data.playlists
+    })
 
     return {
-      style
+      style,
+      playlistArr
     }
   }
 })
 </script>
 
 <style lang="scss" module>
+.content {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 30px 20px;
+}
 </style>
